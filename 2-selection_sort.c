c7af10598f3d66e4d47c4e559963f3f5ff761b46
Abdelhay12 <ettaouafabdelhay87@gmail.com>
2023-07-21 19:08:25 -0700

    updated

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,12 +8,13 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	int tmp;
 	size_t i, j, min_idx;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
@@ -22,12 +23,10 @@ void selection_sort(int *array, size_t size)
 			{
 				min_idx = j;
 			}
-        	}
+		}
 
 		if (i != min_idx)
 		{
-			int tmp;
-
 			tmp = array[i];
 			array[i] = array[min_idx];
 			array[min_idx] = tmp;
