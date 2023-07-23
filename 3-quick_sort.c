52fbc070c4e727a4ce215795b2f09433b526b976
Abdelhay12 <ettaouafabdelhay87@gmail.com>
2023-07-22 19:10:02 -0700

    creates a function that sorts an array of integers in ascending order using the Quick sort algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,79 @@
+#include "sort.h"
+
+/**
+ * swap - swaps two elements
+ * @xp: first element
+ * @yp: second element
+ * Return: void
+ */
+void swap(int *xp, int *yp)
+{
+    int temp;
+
+    temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+/**
+ * partition - implement the Lomuto partition scheme.
+ * @array: array of integers
+ * @l_idx: the start point
+ * @u_idx: the end point
+ * Return: the pivot
+ */
+int partition(int *array, size_t l_idx, size_t u_idx)
+{
+    int pivot;
+    size_t start, end;
+
+    pivot = array[l_idx];
+    start = l_idx;
+    end = u_idx;
+    while (start < end)
+    {
+        while (array[start] <= pivot)
+            start++;
+
+        while (array[end] >= pivot)
+            end--;
+
+        if (start < end)
+            swap(&array[start], &array[end]);
+    }
+
+    swap(&array[l_idx], &array[end]);
+    return end;
+}
+
+/**
+ * quickSort - sortes the element in an array using the Lomuto partition
+ * @array: array to be sorted
+ * @l_idx: the start point
+ * @u_idx: the end point
+ * Return: void
+ */
+void quickSort(int *array, size_t l_idx, size_t u_idx)
+{
+    size_t loc;
+
+    if (l_idx < u_idx)
+    {
+        loc = partition(array, l_idx, u_idx);
+        quickSort(array, l_idx, loc - 1);
+        quickSort(array, loc + 1, u_idx);
+    }
+}
+
+/**
+ * quick_sort - calls the function quickSort
+ * @array: array of integers to be sorted
+ * @size: the length of the array
+ * Return: void
+ */
+void quick_sort(int *array, size_t size)
+{
+    if (array == NULL || size < 2)
+        return;
+    quickSort(array, 0, size - 1);
+}
